a8b8e8fbbb6ebb929ae28fd8ce7eb56078dc6145
Zachary Haber <zachary.haber@abbvie.com>
2021-08-13 16:37:30 -0500

    fix: `toBindDefs` will now look through all rows for the type
    If the first row had a null value, then previously that bind key would be considered null
    
    closes issue #4

--- src/lib/toBindDefs.ts
+++ src/lib/toBindDefs.ts
@@ -1,20 +1,37 @@
-import OracleDB, { BindDefinition } from 'oracledb';
+import {
+  BIND_IN,
+  BindDefinition,
+  BUFFER,
+  DATE,
+  DEFAULT,
+  NUMBER,
+  STRING,
+} from 'oracledb';
 import { Value } from './sql';
 
-function getTypeFromValue(value?: Value) {
-  if (typeof value === 'string') {
-    return OracleDB.STRING;
+type Values = Record<string, Value>[];
+
+function getTypeFromValue(key: string, values: Values): number {
+  for (let i = 0; i < values.length; ++i) {
+    const value = values[i][key];
+    if (value == null) {
+      continue;
+    }
+    const type = typeof value;
+    if (type === 'string') {
+      return STRING;
+    }
+    if (type === 'number' || type === 'bigint') {
+      return NUMBER;
+    }
+    if (value instanceof Date) {
+      return DATE;
+    }
+    if (value instanceof Buffer) {
+      return BUFFER;
+    }
   }
-  if (typeof value === 'number' || typeof value === 'bigint') {
-    return OracleDB.NUMBER;
-  }
-  if (value instanceof Date) {
-    return OracleDB.DATE;
-  }
-  if (value instanceof Buffer) {
-    return OracleDB.BUFFER;
-  }
-  return OracleDB.DEFAULT;
+  return DEFAULT;
 }
 
 function getMaxSize(
@@ -23,13 +40,13 @@ function getMaxSize(
   values: Record<string, Value>[]
 ): number | undefined {
   switch (type) {
-    case OracleDB.BUFFER:
+    case BUFFER:
       return Math.max(
         ...values.map((valueObj) => {
           return (valueObj[key] as Buffer)?.byteLength ?? null;
         })
       );
-    case OracleDB.STRING:
+    case STRING:
       return Math.max(
         ...values.map((valueObj) => {
           return Buffer.byteLength((valueObj[key] as string) ?? '', 'utf-8');
@@ -62,12 +79,11 @@ export function toBindDefs(
   const valuesArr = Array.isArray(valueOrValues)
     ? valueOrValues
     : [valueOrValues];
-  const firstValue = valuesArr[0];
-  for (const key of Object.keys(firstValue)) {
-    const type = overrides[key]?.type ?? getTypeFromValue(firstValue[key]);
+  for (const key of Object.keys(valuesArr[0])) {
+    const type = overrides[key]?.type ?? getTypeFromValue(key, valuesArr);
     defs[key] = {
       ...overrides[key],
-      dir: overrides[key]?.dir ?? OracleDB.BIND_IN,
+      dir: overrides[key]?.dir ?? BIND_IN,
       type,
       maxSize: overrides[key]?.maxSize ?? getMaxSize(key, type, valuesArr),
     };
